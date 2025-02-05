23080dfe897fae132cbe31ec7d5ccc9287657f4e
Diogo Gaspar <dgaspar@kth.se>
2025-02-05 12:19:44 +0100

    fix: switch ln to 2 cp's

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,7 @@ fi
 # Change to the tool directory
 cd tool/
 # This should allow for caching to be retrieved across runs
-ln -s "$GITHUB_WORKSPACE/tool/cache" cache/
+cp -r "$GITHUB_WORKSPACE/tool/cache" cache/
 
 # Build the command
 CMD="python main.py -p ${PROJECT_REPO} -v ${VERSION_OLD} -s -pm ${PACKAGE_MANAGER}"
@@ -60,6 +60,8 @@ fi
 
 echo "Running command: $CMD"
 eval $CMD
+
+cp -r cache/ "$GITHUB_WORKSPACE/tool/cache"
 
 # Check if any reports were generated
 if [ ! -d "results" ]; then
